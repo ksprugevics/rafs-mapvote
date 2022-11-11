8bd8a4a359056be73e3db4178595f0acb9d34048
klavssprugevics <klavs.sprugevics@gmail.com>
2022-11-11 20:13:49 +0200

    Font

--- lua/client/gui/rmv_fonts.lua
+++ lua/client/gui/rmv_fonts.lua
@@ -1,44 +1,40 @@
-if CLIENT then
+surface.CreateFont("TitleFont", {
+    font = "Trebuchet24",
+    extended = true,
+    size = 40,
+    weight = 900,
+    antialias = true,
+})
 
-    surface.CreateFont('TitleFont', {
-        font = 'Trebuchet24',
-        extended = true,
-        size = 40,
-        weight = 900,
-        antialias = true,
-    })
+surface.CreateFont("ButtonFont", {
+    font = "Trebuchet24",
+    extended = true,
+    size = 30,
+    weight = 800,
+    antialias = true,
+})
 
-    surface.CreateFont('ButtonFont', {
-        font = 'Trebuchet24',
-        extended = true,
-        size = 30,
-        weight = 800,
-        antialias = true,
-    })
+surface.CreateFont("XFont", {
+    font = "Trebuchet24",
+    extended = true,
+    size = 40,
+    weight = 6000,
+    antialias = true,
+})
 
-    surface.CreateFont('XFont', {
-        font = 'Trebuchet24',
-        extended = true,
-        size = 40,
-        weight = 6000,
-        antialias = true,
-    })
+surface.CreateFont("TextOverImageFont", {
+    font = "Trebuchet24",
+    extended = true,
+    size = 30,
+    weight = 600,
+    antialias = true,
+    shadow = true
+})
 
-    surface.CreateFont('TextOverImageFont', {
-        font = 'Trebuchet24',
-        extended = true,
-        size = 30,
-        weight = 600,
-        antialias = true,
-        shadow = true
-    })
-
-    surface.CreateFont('Version font', {
-        font = 'Trebuchet24',
-        extended = true,
-        size = 15,
-        weight = 400,
-        antialias = true,
-    })
-
-end
+surface.CreateFont("Version font", {
+    font = "Trebuchet24",
+    extended = true,
+    size = 15,
+    weight = 400,
+    antialias = true,
+})
