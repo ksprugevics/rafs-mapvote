a1e8cea892409ed4aa268be3cc26499c33dd5e7e
klavssprugevics <klavs.dev@gmail.com>
2024-01-01 15:01:15 +0200

    Better time spacing between info messages #33

--- lua/client/rmv_chat_routines.lua
+++ lua/client/rmv_chat_routines.lua
@@ -2,7 +2,7 @@ local function chatTimers()
     timer.Create("RMVCHATMAPRTV", 300, 0, function()
         LocalPlayer():ChatPrint("[RMV] You can rock the vote by typing !rtv")
     end)
-    timer.Create("RMVCHATMAPPOOL", 500, 3, function()
+    timer.Create("RMVCHATMAPPOOL", 721, 3, function()
         LocalPlayer():ChatPrint("[RMV] You can view the server's map pool by typing !rmvlist")
     end)
 end
